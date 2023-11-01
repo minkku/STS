99da1ed05553478f7af2d02e184e379655c2172f
minkku <0106995@naver.com>
2023-11-01 11:34:41 +0900

    Spring first

---
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,17 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import com.sun.jdi.connect.spi.Connection;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	
+
+	}
