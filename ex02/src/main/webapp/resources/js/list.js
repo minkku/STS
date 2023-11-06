abd43302567ae89ba26738b8c9293f3fd03fa38a
minkku <0106995@naver.com>
2023-11-06 15:28:53 +0900

    wip

--- ex02/src/main/webapp/resources/js/list.js
+++ ex02/src/main/webapp/resources/js/list.js
@@ -20,3 +20,25 @@ $(document).ready(function() {
 				self.location = "/board/register";
 		});
 });
+
+$(document).ready(function() {
+		var formObj = $("form");
+		
+		$('button').on("click", function(e) {
+		
+		e.preventDefault();
+		
+		var operation = $(this).data("oper");
+		
+		console.log(operation);
+		
+		if(operation === 'remove') {
+			formObj.attr("action", "/board/remove");
+		} else if(operation === 'list') {
+			//move to list
+			formObj.attr("action", "/board/list").attr("method", "get");
+			formObj.empty();
+		}
+			formObj.submit();
+	});
+});
